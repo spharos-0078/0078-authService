530889d156a95391acc8bb0695f6afa5567ea1e7
JungDongSeop <ds123911@gmail.com>
2025-05-26 18:39:41 +0900

    [auth] feat: 문자 요청 api 작성 완료

--- src/main/java/com/pieceofcake/auth_service/common/config/RedisConfig.java
+++ src/main/java/com/pieceofcake/auth_service/common/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.pieceofcake.auth_service.common.config;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.time.Duration;
 
@@ -18,22 +20,21 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class RedisConfig {
 
-    private final RedisProperties redisProperties;
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        // ACL 유저명과 패스워드
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(redisProperties.getHost());
-        config.setPort(redisProperties.getPort());
-        config.setUsername(redisProperties.getUsername());
-        config.setPassword(redisProperties.getPassword());
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
 
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .commandTimeout(Duration.ofSeconds(2))
-                .build();
-
-        return new LettuceConnectionFactory(config, clientConfig);
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return stringRedisTemplate;
     }
 }
 
--- src/main/java/com/pieceofcake/auth_service/member/application/PhoneServiceImpl.java
+++ src/main/java/com/pieceofcake/auth_service/member/application/PhoneServiceImpl.java
@@ -1,6 +1,9 @@
 package com.pieceofcake.auth_service.member.application;
 
+import com.pieceofcake.auth_service.common.util.SendPhoneCodeUtil;
+import com.pieceofcake.auth_service.common.util.RedisUtil;
 import com.pieceofcake.auth_service.member.dto.in.SendPhoneCodeRequestDto;
+import com.pieceofcake.auth_service.member.infrastructure.SendPhoneCodeDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +11,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PhoneServiceImpl implements PhoneService{
 
+    private final RedisUtil redisUtil;
+    private final SendPhoneCodeUtil sendPhoneCodeUtil;
+    private final SendPhoneCodeDao sendPhoneCodeDao;
+
     @Override
     public void sendPhoneCode(SendPhoneCodeRequestDto sendPhoneCodeRequestDto) {
-        System.out.println("Sending phone code for: " + sendPhoneCodeRequestDto.getPhoneNumber());
+        String phoneNumber = sendPhoneCodeRequestDto.getPhoneNumber();
+        int randomNumber = (int) (Math.random() * 900000) + 100000; // 6자리 랜덤 숫자 생성
+        String certificationNumber = String.valueOf(randomNumber);
+        sendPhoneCodeUtil.sendSms(phoneNumber, certificationNumber);
+        sendPhoneCodeDao.createSmsCertification(phoneNumber, certificationNumber);
     }
 }
