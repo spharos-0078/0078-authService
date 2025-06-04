2c1da67c6390351efaaf20f64e5efe136827f4f0
Eunseo <kendyjenny@naver.com>
2025-06-04 14:35:58 +0900

    [auth] fix: SecurityConfig에 swagger 필터 추가

--- src/main/java/com/pieceofcake/auth_service/common/config/SecurityConfig.java
+++ src/main/java/com/pieceofcake/auth_service/common/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/**",
+                                "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html")
                         .permitAll()
